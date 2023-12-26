74579ba91224873e5ef7337bb7fb73473a69fc83
usss <88474883+sukimumuu@users.noreply.github.com>
2023-12-26 15:59:11 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,16 @@ use App\Http\Controllers\TaskController;
 
 
 Route::get('/', [PageController::class, 'index'])->name('index');
-Route::get('/login/{id}', [PageController::class, 'login'])->name('login');
 Route::post('/login-auth', [AuthController::class, 'login'])->name('login-auth');
+
+Route::middleware(['auth'])->group(function () {
 Route::get('/create-profil', [AuthController::class, 'index'])->name('index-create-profil');
 Route::post('/store-profil', [AuthController::class, 'store'])->name('store-profil');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/login/{id}', [PageController::class, 'login'])->name('login');
 Route::get('/home', [PageController::class,'home'])->name('home');
 Route::get('/buat-task', [PageController::class,'task'])->name('task');
 Route::post('/buat-task', [TaskController::class,'create'])->name('task-create');
 Route::patch('/complete-task/{id}', [TaskController::class, 'completeTask'])->name('complete-task');
 Route::get('/destroy/{id}', [TaskController::class, 'destroy'])->name('destroy');
+});
